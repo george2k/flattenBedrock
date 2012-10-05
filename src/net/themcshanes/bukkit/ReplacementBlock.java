8f9bf4661cf84b556a8c62e6c8521f1fdb7ae437
James McShane <james@themcshanes.net>
2012-10-04 17:14:42 -0700

    Initial Release (0.0.0.1)
    Initial version that works

---
+++ src/net/themcshanes/bukkit/ReplacementBlock.java
@@ -0,0 +1,12 @@
+package net.themcshanes.bukkit;
+
+public class ReplacementBlock {
+	public int BlockID;
+	public byte DataValue;
+	
+	public ReplacementBlock(int blockId, byte dataValue)
+	{
+		BlockID = blockId;
+		DataValue = dataValue;
+	}
+}
